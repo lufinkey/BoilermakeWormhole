f1ee653268135f7b6adc5f9e71c0f00ede3d2526
Matthew Pace <pace4@purdue.edu>
2014-10-18 12:57:11 -0400

    Fixed everything, ty

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 
 #undef main
 
-#ifdef SFML_STATIC
+/*#ifdef SFML_STATIC
 #pragma comment(lib, "glew.lib")
 #pragma comment(lib, "freetype.lib")
 #pragma comment(lib, "jpeg.lib")
 #pragma comment(lib, "opengl32.lib")
 #pragma comment(lib, "winmm.lib")
-#pragma comment(lib, "gdi32.lib")  
+#pragma comment(lib, "gdi32.lib")
 #pragma comment(lib, "ws2_32.lib")
-#endif // SFML_STATIC
+#endif // SFML_STATIC*/
 
 int main(int argc, char* argv[])
 {
