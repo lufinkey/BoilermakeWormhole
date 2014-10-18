69487050d3caed3d161f49d418461adcf0c8e42a
lufinkey <luisfinke@gmail.com>
2014-10-18 10:16:00 -0400

    fixed SDL issue
    moved SDL2.dll to root project directory

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 
+#include <SDL.h>
+#undef main
+
 #include <SFML/Network.hpp>
 #include <iostream>
 #include "String.h"
@@ -16,4 +19,20 @@
 int main(int argc, char* argv[])
 {
 	//TODO add bending of space and time
+
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+
+	if(!SDL_WasInit(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER | SDL_INIT_JOYSTICK | SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER))
+	{
+		if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER | SDL_INIT_JOYSTICK | SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER) < 0)
+		//if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+		{
+			return 1;
+		}
+	}
+
+	window = SDL_CreateWindow(NULL, 0, 0, 300, 300, SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS | SDL_WINDOW_SHOWN);
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
